8a20692212c036666be034e36fcb6285817d0488
bnogimauemura <bnogimauemurak@icloud.com>
2025-03-07 10:27:33 +0900

    feat: Create page.tsx inside user-profile/test folder

---
+++ app/user-profile/test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import UserProfile from "@/components/user-profile";
+
+export default async function Home() {
+  return (
+    <>
+      <UserProfile />
+    </>
+  );
+}
