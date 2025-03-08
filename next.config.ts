68dcfd9ca83ecdf2c408522247bb1b8f05c1cdd6
Joe Reeve <joe.reeve@gmail.com>
2025-03-08 19:58:17 +0900

    feat: display food truck information from the database on the landing page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // allows us to load images from out supabase bucket
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qieslzondvbkbokewujq.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
