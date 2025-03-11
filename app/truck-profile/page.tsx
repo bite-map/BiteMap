863080bc13708ae3e3fbba8dda32be592b8de08d
Joe Reeve <joe.reeve@gmail.com>
2025-03-11 16:16:12 +0900

    feat: add new route folder for truck profile
    Added a new route folder for truck profile and added some placeholder information to the food-truck-profile component

---
+++ app/truck-profile/page.tsx
@@ -0,0 +1,10 @@
+import FoodTruckProfile from "@/components/food-truck/food-truck-profile";
+import Map from "@/components/map/map";
+
+export default async function FoodTruck() {
+  return (
+    <>
+      <FoodTruckProfile />
+    </>
+  );
+}
--- components/food-truck/food-truck-profile.tsx
+++ components/food-truck/food-truck-profile.tsx
@@ -1,25 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-// this is temperary component for displying in map when marker clicked
-type FoodTruckProfileProps = {
-  // location
-  // review
-  setTruckProfile: Function;
-};
 
-export default function FoodTruckProfile({
-  setTruckProfile,
-}: FoodTruckProfileProps) {
+// type FoodTruckProfileProps = {
+//   // location
+//   // review
+//   setTruckProfile: Function;
+// };
+
+export default function FoodTruckProfile({}) {
   return (
-    <div className="w-5/6 h-5/6 bg-gray-50">
-      <button
-        onClick={() => {
-          setTruckProfile(false);
-        }}
-      >
-        Close
-      </button>
-      <button>Add to Favourite</button>
-      <button>Sight</button>
+    <div className="rounded-xl bg-background overflow-clip shadow-md">
+      <Image
+        className="h-[200px] object-cover"
+        src={
+          "https://qieslzondvbkbokewujq.supabase.co/storage/v1/object/public/BiteMap//foodtruck.webp"
+        }
+        alt="Picture of a food truck"
+        width={600}
+        height={600}
+      ></Image>
+      <div className="flex">
+        <div className="p-3">
+          <h2 className="text-lg font-semibold text-primary">{"Test Truck"}</h2>
+          <p>{"Test Style"}</p>
+        </div>
+      </div>
     </div>
   );
 }
