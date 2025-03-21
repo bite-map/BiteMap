1a17709e4d04aa2746f11e6faf5222cfe194d67d
Joe Reeve <joe.reeve@gmail.com>
2025-03-21 14:39:41 +0900

    feat: make sighting link the whole div

--- components/food-truck/sighting-card.tsx
+++ components/food-truck/sighting-card.tsx
@@ -5,14 +5,16 @@ import Link from "next/link";
 import { GiConfirmed } from "react-icons/gi";
 import { FaSpinner } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
-
 import { usePathname } from "next/navigation";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 type SightingCardProps = {
   sightingData: any;
 };
 
 export default function SightingCard({ sightingData }: SightingCardProps) {
+  dayjs.extend(relativeTime);
   const pathname = usePathname();
   const [localTime, setLocalTime] = useState<string>();
 
@@ -34,11 +36,11 @@ export default function SightingCard({ sightingData }: SightingCardProps) {
   }, []);
 
   return (
-    <div className="flex rounded-xl bg-background overflow-clip shadow-md ring-1 ring-primary">
-      <div className="grow overflow-hidden px-2 py-1">
-        {sightingData ? (
-          <div className="flex flex-row ">
-            <Link href={`/truck-map?sighting-id=${sightingData.id}`}>
+    <Link href={`/truck-map?sighting-id=${sightingData.id}`}>
+      <div className="flex rounded-xl bg-background overflow-clip shadow-md ring-1 ring-primary">
+        <div className="grow overflow-hidden px-2 py-1">
+          {sightingData ? (
+            <div className="flex flex-row ">
               <div className="flex flex-col w-80">
                 {sightingData.address_formatted ? (
                   <p className="truncate overflow-auto mb-1">
@@ -53,30 +55,31 @@ export default function SightingCard({ sightingData }: SightingCardProps) {
                   <p>{`Last sighted at: ${localTime}`}</p>
                 )}
               </div>
-            </Link>
-            <div className="flex items-center justify-center w-full">
-              {/* show how many  confirmation the sighting has, may need to change icon if it's confusing */}
-              <p className="text-center mr-1">
-                {sightingData.confirmation_count}
-              </p>
-              <GiConfirmed></GiConfirmed>
+
+              <div className="flex items-center justify-center w-full">
+                {/* show how many  confirmation the sighting has, may need to change icon if it's confusing */}
+                <p className="text-center mr-1">
+                  {sightingData.confirmation_count}
+                </p>
+                <GiConfirmed></GiConfirmed>
+              </div>
             </div>
-          </div>
-        ) : (
-          <p className="flex items-center gap-2 h-full w-full">
-            Loading Sighting{" "}
-            <FaSpinner className="animate-[spin_2s_ease-in-out_infinite] text-primary" />
-          </p>
+          ) : (
+            <p className="flex items-center gap-2 h-full w-full">
+              Loading Sighting{" "}
+              <FaSpinner className="animate-[spin_2s_ease-in-out_infinite] text-primary" />
+            </p>
+          )}
+        </div>
+        {pathname === "/user-profile" && (
+          <Link
+            href={`/truck-profile/${sightingData.food_truck_id}`}
+            className=" flex justify-center items-center text-background text-2xl bg-primary w-20"
+          >
+            <FaArrowRight />
+          </Link>
         )}
       </div>
-      {pathname === "/user-profile" && (
-        <Link
-          href={`/truck-profile/${sightingData.food_truck_id}`}
-          className=" flex justify-center items-center text-background text-2xl bg-primary w-20"
-        >
-          <FaArrowRight />
-        </Link>
-      )}
-    </div>
+    </Link>
   );
 }
