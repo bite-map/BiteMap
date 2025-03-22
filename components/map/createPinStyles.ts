3f8682c417db53a004749fe9fe6cdb199d2c65ff
cabbage <cab226023@gmail.com>
2025-03-22 12:12:13 +0900

    feat: update Pin element UI

--- components/map/createPinStyles.ts
+++ components/map/createPinStyles.ts
@@ -6,8 +6,7 @@
 export function createSelectedLocationPin(google: any) {
   const selectedLocationPin = new google.maps.marker.PinElement({
     scale: 1,
-    background: "#cd6155",
-    borderColor: "#cd6155",
+    background: "#82e0aa",
     glyphColor: "#f9ebea",
   });
   return selectedLocationPin;
@@ -17,10 +16,12 @@ export function createSelectedLocationPin(google: any) {
 // ----- Pin for trucks -----
 export function createTruckPin(google: any) {
   const truckPin = new google.maps.marker.PinElement({
-    scale: 1.3,
-    background: "#f8c471",
-    borderColor: "#af601a",
-    glyphColor: "#af601a",
+    scale: 1,
+
+    background: "#82e0aa",
+
+    borderColor: "#196f3d",
+    glyphColor: "#196f3d",
   });
   return truckPin;
 }
@@ -28,23 +29,23 @@ export function createTruckPin(google: any) {
 
 // ----- Pin for sightings -----
 const activityLevel = {
-  l0: "#eafaf1",
-  l1: "#d5f5e3",
-  l2: "#abebc6",
-  l3: "#82e0aa",
-  l4: "#58d68d",
-  l5: "#2ecc71",
+  l0: "#fef9e7",
+  l1: "#fcf3cf",
+  l2: "#f9e79f",
+  l3: "#f7dc6f",
+  l4: "#f4d03f",
+  l5: "#f1c40f",
 };
 // TODO: change sighting by frequency, calculate a relative number by ranking in whole table(which sighitng is the most popular one), and send that rate within route to frontend
 // temperary default activity level as 3
 export function createSightingPin(google: any, activity: number = 3) {
   const sightingPin = new google.maps.marker.PinElement({
-    scale: 1,
+    scale: 1.2,
     background: activityLevel[
       `l${activity}` as keyof typeof activityLevel
     ] as string,
-    borderColor: "#196f3d",
-    glyphColor: "#196f3d",
+    borderColor: "#ca6f1e",
+    glyphColor: "#ca6f1e",
   });
   return sightingPin;
 }
