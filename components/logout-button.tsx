26ad2f8a944e0273a7dad1e7188b9bbb2bfa282d
cabbage <cab226023@gmail.com>
2025-04-20 17:42:19 +0900

    fix: stylize nav bar desktop

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -13,10 +13,10 @@ export default function LogoutButton({ handleToggle }: LogoutButtonProps) {
         signOutAction();
         handleToggle();
       }}
-      className="flex font-semibold items-center w-48 h-12 p-3 mt-3 last:mb-4, md:last:mb-0"
+      className="flex font-semibold items-center w-48 h-12 p-3 mt-3 last:mb-4, md:last:mb-0 md:ring-1 md:ring-primary md:rounded-t-xl  md:justify-center"
     >
       <LuLogOut size={26} />
-      <p className="ml-2">Logout</p>
+      <p className="ml-2 ">Logout</p>
     </button>
   );
 }
--- components/nav-button-mobile.tsx
+++ components/nav-button-mobile.tsx
@@ -24,11 +24,11 @@ export default function NavButtonMobile({
         <Link
           onClick={handleToggle}
           href={NavButton.href}
-          className={`mt-${NavButton.marginTop} last:mb-4 md:last:mb-0 md:mt-0  `}
+          className={`mt-${NavButton.marginTop} last:mb-4 md:last:mb-0 md:mt-0 `}
         >
           <div
             className={clsx(
-              "flex font-semibold items-center w-48 h-12 p-3 mt-3 ",
+              "flex font-semibold items-center w-48 h-12 p-3 mt-3 md:max-w-[] md:justify-center md:p-0  md:rounded-t-xl md:ring-1 md:ring-primary",
               {
                 "bg-primary text-background": pathname === NavButton.href,
               }
@@ -46,7 +46,7 @@ export default function NavButtonMobile({
         >
           <div
             className={clsx(
-              "flex font-semibold items-center w-48 h-12 p-3 mt-3",
+              "flex font-semibold items-center w-48 h-12 p-3 mt-3 md:max-w-1/5 md:justify-center  md:rounded-t-xl md:ring-1 md:ring-primary",
               {
                 "bg-primary text-background": pathname.includes("/truck-map"),
               }
